ce1c106206db8a2b0d7b612fbe23cce54256836e
root <root@master1.k8s>
2018-08-17 16:46:04 +0800

    add some func
    Signed-off-by: root <root@master1.k8s>

--- atomic/atomic.go
+++ atomic/atomic.go
@@ -8,12 +8,16 @@ var (
 	default_atomic_uint64 uint64 = 0
 )
 
-func ResetDefUInt64() {
-	atomic.StoreUint64(&default_atomic_uint64, 0)
-}
-
 func GetDefUInt64() uint64 {
 	return atomic.LoadUint64(&default_atomic_uint64)
+}
+
+func SetDefUInt64(val uint64) {
+	atomic.StoreUint64(&default_atomic_uint64, val)
+}
+
+func ResetDefUInt64() {
+	atomic.StoreUint64(&default_atomic_uint64, 0)
 }
 
 func IncrDefUInt64() {
@@ -24,18 +28,38 @@ func DecrDefUInt64() {
 	atomic.AddUint64(&default_atomic_uint64, ^uint64(0))
 }
 
-func ResetUInt64(num *uint64) {
-	atomic.StoreUint64(num, 0)
+func AddDefUInt64(delta uint64) {
+	atomic.AddUint64(&default_atomic_uint64, delta)
 }
 
-func GetUInt64(num *uint64) uint64 {
-	return atomic.LoadUint64(num)
+func SubtractDefUInt64(delta uint64) {
+	atomic.AddUint64(&default_atomic_uint64, ^uint64(delta-1))
 }
 
-func IncrUInt64(num *uint64) {
-	atomic.AddUint64(num, 1)
+func GetUInt64(addr *uint64) uint64 {
+	return atomic.LoadUint64(addr)
 }
 
-func DecrUInt64(num *uint64) {
-	atomic.AddUint64(num, ^uint64(0))
+func SetUInt64(addr *uint64, val uint64) {
+	atomic.StoreUint64(addr, val)
+}
+
+func ResetUInt64(addr *uint64) {
+	atomic.StoreUint64(addr, 0)
+}
+
+func IncrUInt64(addr *uint64) {
+	atomic.AddUint64(addr, 1)
+}
+
+func DecrUInt64(addr *uint64) {
+	atomic.AddUint64(addr, ^uint64(0))
+}
+
+func AddUInt64(addr *uint64, delta uint64) {
+	atomic.AddUint64(addr, delta)
+}
+
+func SubtractUInt64(addr *uint64, delta uint64) {
+	atomic.AddUint64(addr, ^uint64(delta-1))
 }
